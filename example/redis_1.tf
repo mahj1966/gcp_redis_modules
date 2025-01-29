c9ea17b663775df81745993145b6a02976a041fd
mahj1966 <99661210+mahj1966@users.noreply.github.com>
2025-01-29 15:48:22 +0000

    update start_time

--- example/redis_1.tf
+++ example/redis_1.tf
@@ -1,52 +1,45 @@
 provider "google" {
-  project = "votre_projet_id"
+  project = "my-gcp-project"
   region  = "us-central1"
-  # credentials = file("chemin/vers/ma/clé.json")  # si vous utilisez un compte de service
+  # credentials = file("path/to/service-account.json")
 }
 
 module "redis_instance" {
   source = "./modules/redis"
-
-  project_id    = "votre_projet_id"
+  
+  project_id    = "my-gcp-project"
   region        = "us-central1"
   name          = "my-redis-instance"
-  display_name  = "Redis Production"
-
-  labels = {
-    environment = "production"
-    team        = "backend"
-  }
+  display_name  = "My Redis in Production"
 
   tier            = "STANDARD_HA"
   memory_size_gb  = 2
   redis_version   = "REDIS_6_X"
 
-  redis_configs = {
-    "maxmemory-policy"       = "allkeys-lru"
-    "notify-keyspace-events" = "KEA"
-  }
-
-  authorized_network      = "projects/votre_projet_id/global/networks/my-vpc"
-  reserved_ip_range       = "10.10.0.0/29"
+  authorized_network      = "projects/my-gcp-project/global/networks/my-custom-network"
   connect_mode            = "DIRECT_PEERING"
   transit_encryption_mode = "SERVER_AUTHENTICATION"
   auth_enabled            = true
 
   read_replicas_mode = "READ_REPLICAS_ENABLED"
-  replica_count      = 2
+  replica_count       = 2
 
   maintenance_day        = "TUESDAY"
   maintenance_start_time = "03:00"
 
   persistence_mode        = "RDB"
-  rdb_snapshot_period     = "HOURS"
-  rdb_snapshot_interval   = 6
+  rdb_snapshot_period     = "SIX_HOURS"
+  rdb_snapshot_start_time = "04:00"
+
+  redis_configs = {
+    "maxmemory-policy" = "allkeys-lru"
+  }
 }
 
 output "redis_host" {
-  value = module.redis_instance.host
+  value = module.redis_instance.redis_host
 }
 
 output "redis_port" {
-  value = module.redis_instance.port
+  value = module.redis_instance.redis_port
 }
